1d8bea8e2d9457cd60a89d34309dc3a15bf146c3
illuminumeyes <151844579+illuminumeyes@users.noreply.github.com>
2024-03-21 21:07:59 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer>
+            <div id="left-footer">
+                <h3>Quick Links</h3>
+                <p>
+                <ul>
+                    <li>
+                        <a href="./index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="./about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="./blogs.html">Blogs</a>
+                    </li>
+                    <li>
+                        <a href="./contact.html">Contact Us</a>
+                    </li>
+                </ul>
+                </p>
+            </div>
+            <div id="right-footer">
+                <h3>Follow Us On</h3>
+                <div id="social-media-footer">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+
+
+            </div>
+        </footer>
